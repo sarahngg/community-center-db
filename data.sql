de15a18986ec70ce9c96a2a73be1553f3e14f23f
Sarah Ng <sarah.ng26@gmail.com>
2020-11-16 20:19:43 -0800

    fixed all errors, removed comments

--- data.sql
+++ data.sql
@@ -1,3 +1,5 @@
+-- SET ECHO OFF
+set linesize 400
 drop table Takes;
 drop table Process_Purchase_Membership;
 drop table Uses;
@@ -17,7 +19,7 @@ drop table Class_Leads_R4;
 drop table Class_Leads_R1;
 drop table Employee;
 drop table Customer;
--- select table_name from user_tables;
+select table_name from user_tables;
 
 CREATE TABLE Customer (
   email char(50),
@@ -86,7 +88,7 @@ grant select on Process_Purchase_Membership to public;
 
 CREATE TABLE Has_Room_Booking (
   roomNum int,
-  timeSlot timestamp(0), --24-JAN-12 05.57.12 AM
+  timeSlot timestamp, --24-JAN-12 05.57.12 AM
   classID int,
   "date" date,
   PRIMARY KEY (roomNum, timeSlot),
@@ -152,7 +154,6 @@ grant select on Pays_Payment to public;
 
 select table_name from user_tables;
 
-/* Employee Table Insertions */
 insert into Employee values(184, 'Bob', 'Smith');
 insert into Employee values(42, 'Ronald', 'McDonald');
 insert into Employee values(21, 'Lisa', 'Simpson');
@@ -166,54 +167,45 @@ insert into Employee values(144, 'Justin', 'Trudeau');
 insert into Employee values(199, 'Greedy', 'Manager');
 insert into Employee values(200, 'Anne', 'Rescue');
 
-/* Front_Desk_Staff Table Insertions */
 insert into Front_Desk_Staff values(184, 18.72);
 insert into Front_Desk_Staff values(42, 25.30);
 insert into Front_Desk_Staff values(21, 17.17);
 insert into Front_Desk_Staff values(90, 24.30);
 insert into Front_Desk_Staff values(121, 23.10);
 
-/* Instructor Table Insertions */
 insert into Instructor values(30, 32.33, 'Culinary');
 insert into Instructor values(31, 29.00, 'Yoga');
-insert into Instructor values(88, 38.53, 'IT & Web Development');
+insert into Instructor values(88, 38.53, 'IT/Web Development');
 insert into Instructor values(65, 34.05, 'Pottery');
 insert into Instructor values(200, 50.21, 'CPR');
-/* Unique constraint violated for query below? */
 insert into Instructor values(144, 26.27, 'English'); 
 
-/*Class-Leads Insertions*/
 insert into Class_Leads values(105, date '2020-10-23', 0.30, 'Lecture', 'Food Safe 1', 50, 30);
 insert into Class_Leads values(105, date '2020-10-24', 0.30, 'Lecture', 'Food Safe 2', 50, 30);
 insert into Class_Leads values(232, date '2020-10-23', 0.10, 'Training', 'Intro to CPR', 25, 200);
 insert into Class_Leads values(232, date '2020-10-24', 0.10, 'Training', 'CPR Recertification', 25, 200);
 insert into Class_Leads values(167, date '2020-10-25', 0.30, 'Lecture', 'HTML/CSS Level 1', 50, 88);
-insert into Class_Leads values(78, date '2020-10-23', 0, 'Hands-on', 'Pottery: Beginners', 20, 65);
+insert into Class_Leads values(78, date '2020-10-24', 0, 'Hands-on', 'Pottery: Beginners', 20, 65);
 insert into Class_Leads values(78, date '2020-10-30', 0, 'Hands-on', 'Pottery: Intermediate', 20, 65);
 insert into Class_Leads values(107, date '2020-10-23', 0.20, 'Fitness', 'Yoga', 15, 31);
 insert into Class_Leads values(111, date '2020-10-24', 0.20, 'Fitness', 'Pilates', 15, 31);
 insert into Class_Leads values(202, date '2020-10-25', 0, 'Discussion', 'English for New Canadians', 10, 144);
-/* Parent key not found -> Need an employee, cannot have 0. How should we resolve this?*/
-insert into Class_Leads values(301, date '2020-10-26', 0.10, 'Training', 'Computer for Seniors', 25, 0);
+insert into Class_Leads values(301, date '2020-10-26', 0.10, 'Training', 'Computer for Seniors', 25, 88);
+insert into Class_Leads values(75, date '2020-10-23', 0, 'Discussion', 'Ethics for Beginners', 10, 144);
 
-/* Orders_Equipment Table Insertions */
-/* Needed to remove apostrophe */
 insert into Orders_Equipment values(26623, 9.99, 'Potters Wheel', 280775733, 611, 184); 
 insert into Orders_Equipment values(43241, 6.99, 'Kiln', 568349684, 112, 42);
 insert into Orders_Equipment values(11267, 12.99, 'Meat Thermometer', 928832138, 442, 21);
 insert into Orders_Equipment values(64671, 19.99, 'Large Exercise Mat', 212530810, 31, 90);
 insert into Orders_Equipment values(64672, 15.00, 'Kindle Reader', 831551818, 82, 121);
 
-/* Pays_Payment Table Insertions */
 insert into Pays_Payment values(date '2020-09-30', 432, 30, 323.30);
 insert into Pays_Payment values(date '2020-10-30', 433, 31, 290.00);
 insert into Pays_Payment values(date '2020-10-30', 434, 88, 385.30);
 insert into Pays_Payment values(date '2020-10-30', 435, 65, 340.50);
 insert into Pays_Payment values(date '2020-10-30', 436, 121, 2310.30);
-/* Unique constraint violated?*/
 insert into Pays_Payment values(date '2020-11-15', 437, 65, 202.10); 
 
-/*Customer Table Insertions*/
 insert into Customer values('nathanyan@email.com', 'Nathan', 'Yan');
 insert into Customer values('jdoe@gmail.com', 'John', 'Doe');
 insert into Customer values('smithk@hotmail.com', 'Karen', 'Smith');
@@ -221,69 +213,51 @@ insert into Customer values('rreynolds02@gmail.com', 'Ryan', 'Reynolds');
 insert into Customer values('opwinfrey@email.com', 'Oprah', 'Winfrey');
 insert into Customer values('kelly.smith@hotmail.com', 'Kelly', 'Smith');
 insert into Customer values('rng@ubc.ca', 'Raymond', 'Ng');
-/* Unique constraint violated?*/
 insert into Customer values('eknorr@ubc.ca', 'Ed', 'Knorr');
 
-/*Takes Table Insertions*/
-/* Modified the dates in this table to match actual classes, remember, date is a foreign key!*/
-/* Changed billAmount to match the (1-discount)*classPrice */
+-- Modified the dates in this table to match actual classes, remember, date is a foreign key!
+-- Changed billAmount to match the (1-discount)*classPrice 
 insert into Takes values('smithk@hotmail.com', 105, date '2020-10-23', 99.99); 
 insert into Takes values('rng@ubc.ca', 107, date '2020-10-23', 20.00); 
 insert into Takes values('rng@ubc.ca', 111, date '2020-10-24', 9.60); 
-/* Unique constraint violated?*/
 insert into Takes values('jdoe@gmail.com', 78, date '2020-10-23', 18.50); 
-/* Unique constraint violated?*/
 insert into Takes values('jdoe@gmail.com', 78, date '2020-10-30', 18.50); 
 
-/*Process-Purchase-Membership Insertions*/
 insert into Process_Purchase_Membership values(1, 50, 'nathanyan@email.com', date '2020-02-12', 1, 184);
 insert into Process_Purchase_Membership values(2, 100, 'jdoe@gmail.com', date '2020-03-29', 2, 42);
 insert into Process_Purchase_Membership values(3, 50, 'rreynolds02@gmail.com', date '2020-05-08', 3, 21);
 insert into Process_Purchase_Membership values(4, 150, 'opwinfrey@email.com', date '2020-06-14', 4, 42);
 insert into Process_Purchase_Membership values(5, 200, 'kelly.smith@hotmail.com', date '2020-06-28', 5, 90);
 insert into Process_Purchase_Membership values(6, 100, 'rng@ubc.ca', date '2020-08-02', 6, 121);
-/* Parent key not found -> cascading error, need to resolve adding Ed into customer table */
-insert into Process_Purchase_Membership values(7, 50, 'eknorr@ubc.ca', date '2020-09-21', 7, 124);
+insert into Process_Purchase_Membership values(7, 50, 'eknorr@ubc.ca', date '2020-09-21', 7, 121);
 
-/*Workshop Insertions*/
-/* Missing expression on date? */
-insert into Workshop values(105, date "2020-10-23", 'Food Safe Level 1', 79.99); 
-/* Missing expression on date? */
-insert into Workshop values(105, date "2020-10-24", 'Food Safe Level 2', 99.99);
-/* Missing expression on date? */ 
-insert into Workshop values(232, date "2020-10-23", 'CPR Level A', 150.00); 
-/* Missing expression on date? */
-insert into Workshop values(232, date "2020-10-24", 'CPR Level C', 150.00); 
-/* Missing expression on date? */
-insert into Workshop values(167, date "2020-10-25", 'HTML/CSS Level 1', 50.00); 
+insert into Workshop values(105, date '2020-10-23', 'Food Safe Level 1', 79.99); 
+insert into Workshop values(105, date '2020-10-24', 'Food Safe Level 2', 99.99);
+insert into Workshop values(232, date '2020-10-23', 'CPR Level A', 150.00); 
+insert into Workshop values(232, date '2020-10-24', 'CPR Level C', 150.00); 
+insert into Workshop values(167, date '2020-10-25', 'HTML/CSS Level 1', 50.00); 
 
-/* Lesson Table Insertions */
 insert into Lesson values(78, date '2020-10-23', 18.50);
 insert into Lesson values(78, date '2020-10-30', 18.50); 
 insert into Lesson values(107, date '2020-10-23', 25.00); 
 insert into Lesson values(111, date '2020-10-24', 12.00); 
-/* Fixed tihs by changing the date! */
+-- Fixed tihs by changing the date!
 insert into Lesson values(202, date '2020-10-25', 0.00); 
 
-/* Uses Table Insertions */
 insert into Uses values(78, date '2020-10-23', 26623); 
 insert into Uses values(78, date '2020-10-30', 43241); 
 insert into Uses values(105, date '2020-10-23', 11267); 
 insert into Uses values(232, date '2020-10-24', 64671); 
-/* Unique constraint violated? */
 insert into Uses values(202, date '2020-10-25', 64672); 
 
-/*Has_Room_Booking Insetions*/
-/* For all of the insertions below, the date is identified as missing an expression, and where date is excluded, as
-in the null case, we have "TIMESTAMP": invalid identifier...so I think I messed up timestamp*/
-insert into Has_Room_Booking values(4, timestamp('2020-10-23 14:00:00'), 105, date "2020-10-23"); 
-insert into Has_Room_Booking values(4, timestamp('2020-10-23 15:00:00'), 105, date "2020-10-23"); 
-insert into Has_Room_Booking values(4, timestamp('2020-10-23 16:00:00'), 105, date "2020-10-23"); 
-insert into Has_Room_Booking values(4, timestamp('2020-10-23 14:00:00'), 105, date "2020-10-24");
-insert into Has_Room_Booking values(4, timestamp('2020-10-23 15:00:00'), 105, date "2020-10-24");
-insert into Has_Room_Booking values(4, timestamp('2020-10-23 16:00:00'), 105, date "2020-10-24"); 
-insert into Has_Room_Booking values(4, timestamp('2020-10-23 17:00:00'), NULL, NULL); 
-insert into Has_Room_Booking values(11, timestamp('2020-10-23 14:00:00'), 75, date "2020-10-23"); 
-insert into Has_Room_Booking values(2, timestamp('2020-10-23 10:00:00'), 78, date "2020-10-23"); 
-insert into Has_Room_Booking values(2, timestamp('2020-10-30 10:00:00'), 78, date "2020-10-30"); 
-insert into Has_Room_Booking values(5, timestamp('2020-10-23 16:00:00'), 107, date "2020-10-23");
+insert into Has_Room_Booking values(4, timestamp '2020-10-23 14:00:00', 105, date '2020-10-23'); 
+insert into Has_Room_Booking values(4, timestamp '2020-10-23 15:00:00', 105, date '2020-10-23'); 
+insert into Has_Room_Booking values(4, timestamp '2020-10-23 16:00:00', 105, date '2020-10-23'); 
+insert into Has_Room_Booking values(4, timestamp '2020-10-24 14:00:00', 105, date '2020-10-24');
+insert into Has_Room_Booking values(4, timestamp '2020-10-24 15:00:00', 105, date '2020-10-24');
+insert into Has_Room_Booking values(4, timestamp '2020-10-24 16:00:00', 105, date '2020-10-24'); 
+insert into Has_Room_Booking values(4, timestamp '2020-10-24 17:00:00', NULL, NULL); 
+insert into Has_Room_Booking values(11, timestamp '2020-10-23 14:00:00', 75, date '2020-10-23'); 
+insert into Has_Room_Booking values(2, timestamp '2020-10-24 10:00:00', 78, date '2020-10-24'); 
+insert into Has_Room_Booking values(2, timestamp '2020-10-30 10:00:00', 78, date '2020-10-30'); 
+insert into Has_Room_Booking values(5, timestamp '2020-10-23 16:00:00', 107, date '2020-10-23');
