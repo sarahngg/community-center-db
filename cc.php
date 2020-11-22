63f42d7c353c526b6b6d76f41a06b324c7856786
Aleem Tariq <aleemtariq19@gmail.com>
2020-11-22 14:39:01 -0800

    Baseline commit.

--- cc.php
+++ cc.php
@@ -164,7 +164,7 @@
 
             // Your username is ora_(CWL_ID) and the password is a(student number). For example, 
 			// ora_platypus is the username and a12345678 is the password.
-            $db_conn = OCILogon("ora_cwl", "password", "dbhost.students.cs.ubc.ca:1522/stu");
+            $db_conn = OCILogon("ora_aleemtq", "a10417153", "dbhost.students.cs.ubc.ca:1522/stu");
 
             if ($db_conn) {
                 debugAlertMessage("Database is Connected");
